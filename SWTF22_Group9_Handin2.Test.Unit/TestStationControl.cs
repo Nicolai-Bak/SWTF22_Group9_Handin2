c5962a40a7aefccf133e8fa0c60bde09dff19484
Nicolai-Bak <nicolai.bak@gmail.com>
2022-03-17 20:48:14 +0100

    Tilføjelse af testklasse til StationControl

---
+++ SWTF22_Group9_Handin2.Test.Unit/TestStationControl.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Ladeskab.Test
+{
+    [TestFixture]
+    public class TestStationControl
+    {
+        private StationControl _uut;
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new StationControl();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            Assert.Pass();
+        }
+
+    }
+
+}
--- SWTF22_Group9_Handin2.Test.Unit/TestUsbChargerSimulator.cs
+++ SWTF22_Group9_Handin2.Test.Unit/TestUsbChargerSimulator.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace UsbSimulator.Test
 {
+
     [TestFixture]
     public class TestUsbChargerSimulator
     {

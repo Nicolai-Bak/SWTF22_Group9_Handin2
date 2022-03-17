b0180134f9240b71e6673d8b59562b48b9fc1c5e
Nicolai-Bak <nicolai.bak@gmail.com>
2022-03-17 06:16:20 +0100

    Add project files.

---
+++ SWTF22_Group9_Handin2.Test.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SWTF22_Group9_Handin2.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

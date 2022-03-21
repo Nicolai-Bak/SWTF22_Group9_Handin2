05d178fbc2d0082375879161209e33156a3186fb
Anton Beck <anto2238@gmail.com>
2022-03-21 16:56:36 +0100

    Added test for display

---
+++ SWTF22_Group9_Handin2.Test.Unit/TestDisplay.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SWTF22_Group9_Handin2_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace SWTF22_Group9_Handin2.Test.Unit;
+
+[TestFixture]
+public class TestDisplay
+{
+    private IDisplay _display;
+    [SetUp]
+    public void Setup()
+    {
+        _display = Substitute.For<IDisplay>();
+    }
+    
+    [Test]
+    public void Display_DisplayMsg_Prints_String()
+    {
+        var msg = "Test";
+        _display.DisplayMsg(msg);
+        _display.Received(1).DisplayMsg(msg);
+    }
+  
+}
--- SWTF22_Group9_Handin2_ClassLibrary/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SWTF22_Group9_Handin2_ClassLibrary;
-
-public class Class1
-{
-    
-}

be1681cc00ab977118415194d4e68f3ce0ae201e
Nicolai-Bak <nicolai.bak@gmail.com>
2022-03-21 19:58:05 +0100

    tests defined, not implemented

--- SWTF22_Group9_Handin2.Test.Unit/TestStationControl.cs
+++ SWTF22_Group9_Handin2.Test.Unit/TestStationControl.cs
@@ -1,24 +1,97 @@
-﻿using NUnit.Framework;
+﻿using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using NSubstitute;
+using NUnit.Framework;
+using SWTF22_Group9_Handin2_ClassLibrary;
 
-namespace Ladeskab.Test
+namespace SWTF22_Group9_Handin2.Test.Unit
 {
     [TestFixture]
     public class TestStationControl
     {
         private StationControl _uut;
+
+        private IChargeControl _chargeControl;
+        private IDoor _door;
+        private ILog _log;
+        private IDisplay _display;
+        private IRfidReader _rfidReader;
+
+
         [SetUp]
         public void Setup()
         {
-            _uut = new StationControl();
+            _chargeControl = Substitute.For<IChargeControl>();
+            _door = Substitute.For<IDoor>();
+            _log = Substitute.For<ILog>();
+            _display = Substitute.For<IDisplay>();
+
+            _uut = new StationControl(_chargeControl, _door, _log, _display);
+
+            _rfidReader = Substitute.For<IRfidReader>();
         }
 
-        [Test]
-        public void Test1()
-        {
+        /* Scenarier:
+        RfidEvent - Locked - Korrekt ID - Assert: 
+        RfidEvent - Locked - Korrekt ID - Assert: 
+         
+         */
 
+        [TestCase(10)]
+        public void RfidEvent_StateAvailable_PhoneConnected(int id)
+        {
+            //Assert: tjek oldid, lockdoor kaldt, startcharging kaldt, log kaldt, state opdatert til locked, display kaldt
+            _rfidReader.RfidEvent += Raise.EventWith(new RfidEventArgs {Id = id});
             Assert.Pass();
         }
-
+        [TestCase(10)]
+        public void RfidEvent_StateAvailable_PhoneNotConnected(int id)
+        {
+            //Assert: display kaldt, state uændret
+            _rfidReader.RfidEvent += Raise.EventWith(new RfidEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void RfidEvent_StateLocked_IdCorrect(int id)
+        {
+            //Assert: stopcharging kaldt, unlockdoor kaldt, log kaldt, display kaldt
+            _rfidReader.RfidEvent += Raise.EventWith(new RfidEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void RfidEvent_StateLocked_IdIncorrect(int id)
+        {
+            //Assert: dislpay kaldt
+            _rfidReader.RfidEvent += Raise.EventWith(new RfidEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void RfidEvent_StateDoorOpen_NoAction(int id)
+        {
+            //Assert: der skal ikke ske noget
+            _rfidReader.RfidEvent += Raise.EventWith(new RfidEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void DoorEvent_StateAvailable(int id)
+        {
+            //Assert: dislay kaldt
+            _door.DoorEvent += Raise.EventWith(new DoorEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void DoorEvent_StateLocked(int id)
+        {
+            //Assert: display kaldt
+            _door.DoorEvent += Raise.EventWith(new DoorEventArgs { Id = id });
+            Assert.Pass();
+        }
+        [TestCase(10)]
+        public void DoorEvent_StateDoorOpen(int id)
+        {
+            //Assert: der skal ikke ske noget
+            _door.DoorEvent += Raise.EventWith(new DoorEventArgs { Id = id });
+            Assert.Pass();
+        }
     }
 
 }
--- SWTF22_Group9_Handin2_ClassLibrary/StationControl.cs
+++ SWTF22_Group9_Handin2_ClassLibrary/StationControl.cs
@@ -73,16 +73,14 @@ namespace SWTF22_Group9_Handin2_ClassLibrary
         public ILog Log { get; set; }
         public IDisplay Display { get; set; }
 
-        public StationControl(IChargeControl charger,
-                              IDoor door,
-                              ILog log,
-                              IDisplay display)
+        public StationControl(IChargeControl charger, IDoor door, ILog log, IDisplay display)
         {
             Charger = charger;
             Door = door;
             Log = log;
             Display = display;
-
+            State = new Available();
+            OldId = 0;
             Door.UnlockDoor();
             Display.DisplayMsg("Indlæs RFID");
         }
@@ -91,13 +89,13 @@ namespace SWTF22_Group9_Handin2_ClassLibrary
         // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
         private void OnRfidEvent(object o, RfidEventArgs args)
         {
-            State.HandleRfidEvent(this, RfidEventArgs args);
+            State.HandleRfidEvent(this, args);
         }
         //
         //         // Her mangler de andre trigger handlere
         private void OnDoorEvent(DoorEventArgs args)
         {
-            State.HandleDoorEvent(this, DoorEventArgs args);
+            State.HandleDoorEvent(this, args);
         }
     }
 }

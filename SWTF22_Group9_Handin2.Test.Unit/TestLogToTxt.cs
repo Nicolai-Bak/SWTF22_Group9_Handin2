b6fb83152d8d21af93d29464fc41c76ed7565fe2
Aksel <akselb@live.dk>
2022-03-22 14:02:09 +0100

    Added log and tests for it

---
+++ SWTF22_Group9_Handin2.Test.Unit/TestLogToTxt.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using SWTF22_Group9_Handin2_ClassLibrary;
+
+namespace SWTF22_Group9_Handin2.Test.Unit;
+
+[TestFixture]
+public class TestLogToTxt
+{
+    private LogToTxt _uut;
+    [SetUp]
+    public void Setup()
+    {
+        _uut = new LogToTxt();
+    }
+    
+    [Test]
+    public void SimulateLogDoorLocked_FileLengthIncreased()
+    {
+        string previousLog = ReadLog();
+        
+        _uut.LogDoorLocked(1);
+        
+        string newLog = ReadLog();
+        
+        Assert.That(newLog.Length, Is.GreaterThan(previousLog.Length));
+    }
+    
+    [Test]
+    public void SimulateLogDoorUnlocked_FileLengthIncreased()
+    {
+        string previousLog = ReadLog();
+        
+        _uut.LogDoorUnlocked(1);
+        
+        string newLog = ReadLog();
+        
+        Assert.That(newLog.Length, Is.GreaterThan(previousLog.Length));
+    }
+
+    private string ReadLog()
+    {
+        string log = "";
+        
+        using (StreamReader r = File.OpenText("log.txt"))
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                log = log.Insert(log.Length, line);
+            }
+        }
+        
+        return log;
+    }
+}

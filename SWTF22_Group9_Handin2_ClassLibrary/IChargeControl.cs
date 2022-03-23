b33341fb157fc6ba3ea71f6847f1490ded74a66c
Anton Beck <anto2238@gmail.com>
2022-03-23 19:42:26 +0100

    Ichargecorrection

--- SWTF22_Group9_Handin2_ClassLibrary/IChargeControl.cs
+++ SWTF22_Group9_Handin2_ClassLibrary/IChargeControl.cs
@@ -1,73 +1,73 @@
-﻿//using System;
+﻿using System;
 
-//namespace UsbSimulator
-//{
-//    public interface IChargeControl
-//    {
-//        void StartCharging();
+namespace UsbSimulator
+{
+    public interface IChargeControl
+    {
+        void StartCharging();
 
-//        void StopCharging();
+        void StopCharging();
 
-//        bool isConnected();
+        bool isConnected();
 
-//        void OnCurrentEvent(object o, CurrentEventsArg args);
-//    }
+        void OnCurrentEvent(object o, CurrentEventsArg args);
+    }
 
-//    public class ChargeControl : IChargeControl
-//    {
-//        private IUSBCharger _usbCharger;
+    public class ChargeControl : IChargeControl
+    {
+        private IUSBCharger _usbCharger;
 
-//        private IDisplay _display;
+        private IDisplay _display;
 
-//        public ChargeControl(IUSBCharger usbCharger, IDisplay display)
-//        {
-//            _usbCharger = usbCharger;
-//            _display = display;
-//        }
+        public ChargeControl(IUSBCharger usbCharger, IDisplay display)
+        {
+            _usbCharger = usbCharger;
+            _display = display;
+        }
 
-//        void IChargeControl.StartCharging()
-//        {
-//            _usbCharger.StartCharge();
-//            _display.DisplayMsg("Enheden oplader");
-//        }
+        void IChargeControl.StartCharging()
+        {
+            _usbCharger.StartCharge();
+            _display.DisplayMsg("Enheden oplader");
+        }
 
-//        void IChargeControl.StopCharging()
-//        {
-//            _usbCharger.StopCharge();
-//        }
+        void IChargeControl.StopCharging()
+        {
+            _usbCharger.StopCharge();
+        }
 
-//        bool IChargeControl.isConnected()
-//        {
-//            if(_usbCharger.Connected)
-//                return true;
-//            else
-//                return false;
-//        }
+        bool IChargeControl.isConnected()
+        {
+            if (_usbCharger.Connected)
+                return true;
+            else
+                return false;
+        }
 
-//        void IChargeControl.OnCurrentEvent(object o, CurrentEventsArg args)
-//        {
-//            switch(args.Current)
-//            {
-//            case 0:
-//                    StopCharging();
-//                    break;
-//            case <= 5:
-//                    StopCharging();
-//                    _display.DisplayMsg("Enheden er fuldt opladt");
-//                    break;
-//            case > 5:
-//                    if(args.Current <= 500)
-//                    {
-//                        StartCharging();
-//                        break;
-//                    }
-//                    else
-//                    {
-//                        StopCharging();
-//                        _display.DisplayMsg("ERROR40: KAN IKKE OPLADES KORREKT");
-//                        break;
-//                    }
-//            }
-//        }
-//    }
-//}
+        void IChargeControl.OnCurrentEvent(object o, CurrentEventsArg args)
+        {
+            switch (args.Current)
+            {
+                case 0:
+                    StopCharging();
+                    break;
+                case <= 5:
+                    StopCharging();
+                    _display.DisplayMsg("Enheden er fuldt opladt");
+                    break;
+                case > 5:
+                    if (args.Current <= 500)
+                    {
+                        StartCharging();
+                        break;
+                    }
+                    else
+                    {
+                        StopCharging();
+                        _display.DisplayMsg("ERROR40: KAN IKKE OPLADES KORREKT");
+                        break;
+                    }
+            }
+        }
+    }
+}

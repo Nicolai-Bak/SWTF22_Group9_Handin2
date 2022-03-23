5d12221db662aef539ff1508af4c77d81ee314dd
Anton Beck <anto2238@gmail.com>
2022-03-23 17:49:18 +0100

    implementation without test

---
+++ SWTF22_Group9_Handin2_ClassLibrary/IDoor.cs
@@ -0,0 +1,20 @@
+﻿namespace SWTF22_Group9_Handin2_ClassLibrary;
+
+public interface IDoor
+{
+    public event EventHandler<DoorEventArgs> DoorEvent;
+
+    public void DoorChangedEvent(DoorEventArgs e);
+
+    public void OnDoorOpen();
+
+    public void OnDoorClose();
+
+    public void LockDoor();
+
+    public void UnlockDoor();
+
+
+
+
+}

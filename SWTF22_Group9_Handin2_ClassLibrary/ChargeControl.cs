c2efcbd783301ded21cb1768dedd05ca77251ce9
Nicolai-Bak <nicolai.bak@gmail.com>
2022-03-24 11:26:41 +0100

    stationcontrol opdelt i filer

---
+++ SWTF22_Group9_Handin2_ClassLibrary/ChargeControl.cs
@@ -0,0 +1,61 @@
+﻿using SWTF22_Group9_Handin2_ClassLibrary;
+
+namespace UsbSimulator;
+
+public class ChargeControl : IChargeControl
+{
+    private IUsbCharger _usbCharger;
+
+    private IDisplay _display;
+
+    public ChargeControl(IUsbCharger usbCharger, IDisplay display)
+    {
+        _usbCharger = usbCharger;
+        _display = display;
+    }
+
+    public void StartCharging()
+    {
+        _usbCharger.StartCharge();
+        _display.DisplayMsg("Enheden oplader");
+    }
+
+    public void StopCharging()
+    {
+        _usbCharger.StopCharge();
+    }
+
+    public bool isConnected()
+    {
+        if(_usbCharger.Connected)
+            return true;
+        else
+            return false;
+    }
+
+    void IChargeControl.OnCurrentEvent(object o, CurrentEventArgs args)
+    {
+        switch(args.Current)
+        {
+            case 0:
+                this.StopCharging();
+                break;
+            case <= 5:
+                StopCharging();
+                _display.DisplayMsg("Enheden er fuldt opladt");
+                break;
+            case > 5:
+                if(args.Current <= 500)
+                {
+                    StartCharging();
+                    break;
+                }
+                else
+                {
+                    StopCharging();
+                    _display.DisplayMsg("ERROR40: KAN IKKE OPLADES KORREKT");
+                    break;
+                }
+        }
+    }
+}
---
+++ SWTF22_Group9_Handin2_ClassLibrary/Locked.cs
@@ -0,0 +1,20 @@
+﻿namespace SWTF22_Group9_Handin2_ClassLibrary;
+
+public class Locked : State
+{
+    public override void HandleRfidEvent(StationControl s, RfidEventArgs a)
+    {
+        if (a.Id == s.OldId)
+        {
+            s.Charger.StopCharging();
+            s.Door.UnlockDoor();
+            s.Log.LogDoorUnlocked(a.Id);
+            s.Display.DisplayMsg("Tag din telefon ud af skabet og luk døren");
+            s.State = new Available();
+        }
+        else
+        {
+            s.Display.DisplayMsg("Forkert RFID tag");
+        }
+    }
+}
